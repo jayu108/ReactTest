40798468631f92172651577d3084616487eb2ab0
park <jayu108@gmail.com>
2016-04-19 16:03:32 +0900

    Added files via upload

---
+++ ReactNativeEx1/index.android.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class TestRN extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>
+          Hello World!
+        </Text>
+        
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  hello: {
+    color: 'blue',
+    backgroundColor: 'yellow',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TestRN', () => TestRN);
